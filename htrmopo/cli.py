054ea09ad8287121e352df663dd1ec229e8f2eec
Benjamin Kiessling <mittagessen@l.unchti.me>
2025-01-01 22:48:46 +0100

    Model publishing/updating

--- htrmopo/cli.py
+++ htrmopo/cli.py
@@ -221,78 +221,31 @@ def get(ctx, model_id):
 @click.pass_context
 @click.option('-i', '--metadata', show_default=True,
               type=click.File(mode='r', lazy=True), help='Model card file for the model.')
+@click.option('-d', '--doi', help='DOI of an existing model. If set the record will be updated by creating a new version.')
 @click.option('-a', '--access-token', prompt=True, help='Zenodo access token')
 @click.option('-p', '--private/--public', default=False, help='Disables Zenodo '
               'community inclusion request. Allows upload of models that will not show '
               'up on `htrmopo list` output')
 @click.argument('model', nargs=1, type=click.Path(exists=False, readable=True, dir_okay=True))
-def publish(ctx, metadata, access_token, private, model):
+def publish(ctx, metadata, doi, access_token, private, model):
     """
     Publishes a model on the Zenodo model repository.
     """
-    import json
+    from htrmopo import publish_model, update_model
 
-    import importlib_resources
-    from jsonschema import validate
-    from jsonschema.exceptions import ValidationError
+    pub_fn = publish_model
 
-    from kraken import repo
-    from kraken.lib import models
-    from kraken.lib.progress import KrakenDownloadProgressBar
-
-    ref = importlib_resources.files('kraken').joinpath('metadata.schema.json')
-    with open(ref, 'rb') as fp:
-        schema = json.load(fp)
-
-    nn = models.load_any(model)
-
-    if not metadata:
-        author = click.prompt('author')
-        affiliation = click.prompt('affiliation')
-        summary = click.prompt('summary')
-        description = click.edit('Write long form description (training data, transcription standards) of the model here')
-        accuracy_default = None
-        # take last accuracy measurement in model metadata
-        if 'accuracy' in nn.nn.user_metadata and nn.nn.user_metadata['accuracy']:
-            accuracy_default = nn.nn.user_metadata['accuracy'][-1][1] * 100
-        accuracy = click.prompt('accuracy on test set', type=float, default=accuracy_default)
-        script = [
-            click.prompt(
-                'script',
-                type=click.Choice(
-                    sorted(
-                        schema['properties']['script']['items']['enum'])),
-                show_choices=True)]
-        license = click.prompt(
-            'license',
-            type=click.Choice(
-                sorted(
-                    schema['properties']['license']['enum'])),
-            show_choices=True)
-        metadata = {
-            'authors': [{'name': author, 'affiliation': affiliation}],
-            'summary': summary,
-            'description': description,
-            'accuracy': accuracy,
-            'license': license,
-            'script': script,
-            'name': os.path.basename(model),
-            'graphemes': ['a']
-        }
-        while True:
-            try:
-                validate(metadata, schema)
-            except ValidationError as e:
-                message(e.message)
-                metadata[e.path[-1]] = click.prompt(e.path[-1], type=float if e.schema['type'] == 'number' else str)
-                continue
-            break
-
-    else:
-        metadata = json.load(metadata)
-        validate(metadata, schema)
-    metadata['graphemes'] = [char for char in ''.join(nn.codec.c2l.keys())]
-    with KrakenDownloadProgressBar() as progress:
-        upload_task = progress.add_task('Uploading', total=0, visible=True if not ctx.meta['verbose'] else False)
-        oid = repo.publish_model(model, metadata, access_token, lambda total, advance: progress.update(upload_task, total=total, advance=advance), private)
-    message('model PID: {}'.format(oid))
+    with open(metadata, 'r') as fp:
+        markdown = fp.read()
+    with Progress() as progress:
+        upload_task = progress.add_task('Uploading', total=0)
+        kwargs = {'model': model,
+                  'model_card': metadata,
+                  'access_token': access_token,
+                  'callback': lambda total, advance: progress.update(upload_task, total=total, advance=advance),
+                  'private': private}
+        if doi:
+            pub_fn = update_model
+            kwargs['model_id'] = doi
+        pub_fn(**kwargs)
+    print(f'model PID: {oid}')
